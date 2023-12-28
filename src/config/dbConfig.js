3d5de3479e54fdf5d219cf14d9411ee8dd642471
Vishal Yadav <68839171+VishalYadav0757@users.noreply.github.com>
2023-12-28 13:15:01 +0530

    Updated db.config file

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "real_estate",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_DBNAME,
   dialect: "mysql",
   pool: {
     max: 5,
